d7d54e7c21bc353d08a2e5d14ff69e149b5c6333
root <dylanbore49@gmail.com>
2021-02-05 03:11:11 +0000

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { FirstComponentComponent } from './first-component/first-component.component';
-import { SecondComponentComponent } from './second-component/second-component.component';
-import { InitialComponentComponent } from './initial-component/initial-component.component';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
 
